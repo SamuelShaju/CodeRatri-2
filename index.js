58303c1ff67415f12395ea11f790bf27fb1bef4a
Rishik131 <86509701+Rishik131@users.noreply.github.com>
2021-08-17 17:23:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,11 +10,38 @@
 
         let now = new Date().getTime(),
             distance = countDown - now;
-
-        document.getElementById("days").innerText = Math.floor(distance / (day))+`  :`;
+          if(Math.floor(distance / (day)) < 10)
+        {
+          document.getElementById("days").innerText = `0`+Math.floor(distance / (day))+`  :`;
+        }
+        else{
+          document.getElementById("days").innerText = Math.floor(distance / (day))+`  :`;
+        }
+        if(Math.floor((distance % (day)) / (hour)) < 10)
+        {
+          document.getElementById("hours").innerText = `0`+Math.floor((distance % (day)) / (hour))+`  :`;
+        }
+        else{
           document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour))+`  :`;
+        }
+        if(Math.floor((distance % (hour)) / (minute)) < 10)
+        {
+          document.getElementById("minutes").innerText = `0`+Math.floor((distance % (hour)) / (minute))+`  :`;
+        }
+        else{
           document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute))+`  :`;
+        }
+        if(Math.floor((distance % (minute)) / second) < 10)
+        {
+          document.getElementById("seconds").innerText = `0`+Math.floor((distance % (minute)) / second);
+        }
+        else{
           document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+        }
+          //document.getElementById("days").innerText = Math.floor(distance / (day))+`  :`;
+          //document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour))+`  :`;
+          //document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute))+`  :`;
+          //document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
         if (distance < 0) {
             clearInterval(x);
             document.getElementById("days").innerText = `00`+`  :`;
